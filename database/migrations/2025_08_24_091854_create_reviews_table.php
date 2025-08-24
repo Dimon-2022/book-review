ebd95f5e62a812545259f6fe83f3553db78a15ef
dima.moisieienko <dimonmail09@gmail.com>
2025-08-24 14:27:00 +0300

    set relationship between two resources: Book & Review

--- database/migrations/2025_08_24_091854_create_reviews_table.php
+++ database/migrations/2025_08_24_091854_create_reviews_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Book;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,9 +14,12 @@ return new class extends Migration
     {
         Schema::create('reviews', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('book_id')->constrained()->cascadeOnDelete();
+            // $table->unsignedBigInteger('book_id');
+            // $table->foreign('book_id')->references('id')->on('books')->onDelete('cascade');
             $table->text('review');
             $table->unsignedTinyInteger('rating');
-            
+
             $table->timestamps();
         });
     }
