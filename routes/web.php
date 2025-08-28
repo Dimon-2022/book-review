e998d11a8f140ed4c86996e96e892471c02fa827
dima.moisieienko <dimonmail09@gmail.com>
2025-08-28 23:07:54 +0300

    resource controller

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,7 @@
 <?php
 
+use App\Http\Controllers\BookController;
 use App\Models\Book;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    $book = Book::find(1);
-    $reviews = $book->reviews;
-
-    dd([
-        'book'=> $book,
-        'reviews' => $reviews
-    ]);
-
-
-
-});
+Route::resource('books', BookController::class);
